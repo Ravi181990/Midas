247ec46218f8b39208131f08b6b4ed92d61f3c6b
Ravi181990 <ravi.saini453@gmail.com>
2018-08-07 18:37:32 +0530

    model change

--- Midas_Demo/Models/SearchModel.cs
+++ Midas_Demo/Models/SearchModel.cs
@@ -1,41 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Midas_Demo.Models
 {
     public class SearchModel
     {
-
-        public int Id { set; get; }
-
-        
+     
         public string Name { set; get; }
 
         public string Description { set; get; }
+
+        public string Frecuency { set; get; }
 
         public string Remaks { set; get; }
 
         public string Tech_Name { set; get; }
 
-        public string Category { set; get; }
+        public List<string> Category { set; get; }
 
         public SelectList CategoryList { set; get; }
 
-        public string Plant { set; get; }
+        public List<string> Plant { set; get; }
 
-         public SelectList PlantList { set; get; }
+        public SelectList PlantList { set; get; }
 
-        public string Transactions { set; get; }
+        public List<string> Transactions { set; get; }
 
         public SelectList TransactionsList { set; get; }
 
-        public string Fields { set; get; }
+        public List<string> Fields { set; get; }
 
         public SelectList FieldsList { set; get; }
 
         public float DashboardVersion { set; get; }
+
     }
 }
